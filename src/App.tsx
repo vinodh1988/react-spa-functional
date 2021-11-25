869b6c2cd33758dad6ca0fa8b6f026a92fe56f5c
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-25 16:15:02 +0530

    child-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Box from './Box';
 
 const App=()=>{
   return (
     <div>
       <h1>React World!!! Welcome</h1>
+      <br/>
+      <Box></Box>
+      <Box></Box>
+      <Box></Box>
     </div>
   )
 }
---
+++ src/Box.tsx
@@ -0,0 +1,10 @@
+
+const Box=()=>{
+    return(
+        <div className="box">
+            <h3>Dummy Title</h3>
+        </div>
+    )
+}
+
+export default Box;
