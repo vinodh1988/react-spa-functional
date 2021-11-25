7ffd68a5131e215ce082b39a96da13b8ce4abaad
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-25 16:49:03 +0530

    use-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import './App.css'
 import Box from './Box';
 
 const App=()=>{
+  const [titles]=useState(['Vegetable','Fruits',"Gadgets","Groceries","Spices","Cosmetics"])
   return (
     <div>
       <h1>React World!!! Welcome</h1>
       <br/>
-      <Box title="Fruits"></Box>
-      <Box title="Vegetables"></Box>
-      <Box title="Gadgets"></Box>
+      {
+        titles.map(x=><Box key={x} title={x}></Box>)
+      }  
     </div>
   )
 }
