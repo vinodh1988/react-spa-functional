198b3c2e1eb6072cd7b069b95ff2e3b452c1194b
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-27 14:49:44 +0530

    event-handlign

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,31 @@ import './App.css'
 import Box from './Box';
 
 const App=()=>{
+  
+  function changeTheme(theme:string):void{
+     setTheme(theme);
+     setButton1(theme=="container1"?"btn btn-danger":"btn btn-light")
+     setButton2(theme=="container2"?"btn btn-danger":"btn btn-light")
+  }
+
   const [titles]=useState(['Vegetable','Fruits',"Gadgets","Groceries","Spices","Cosmetics"])
+  const [button1,setButton1]=useState("btn btn-danger")
+  const [button2,setButton2]=useState("btn btn-ligh")
+  const [theme,setTheme]=useState("container1")
   return (
     <div>
-      <h1>React World!!! Welcome</h1>
+      <h1>React World</h1>
       <br/>
     <div className="alert alert-dark">
-          <button className="btn btn-dark"> Dark Theme</button>&nbsp;&nbsp;
-           <button className="btn btn-dark"> Light Theme</button>
+          <button className={button1} 
+          onClick={()=>{changeTheme("container1")}}> 
+          Dark Theme
+          </button>&nbsp;&nbsp;
+           <button className={button2}
+           onClick={()=>{changeTheme("container2")}}
+           > Light Theme</button>
     </div>
-    <div className="container1" style={{overflow: "hidden"}}>
+    <div className={theme} style={{overflow: "hidden"}}>
       {
         titles.map(x=><Box key={x} title={x}></Box>)
       }  
