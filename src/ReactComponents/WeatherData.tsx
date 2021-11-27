42356cc28e8510b1753f2de6204464219efa06c0
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-27 17:03:54 +0530

    use-effect-axios

--- src/ReactComponents/WeatherData.tsx
+++ src/ReactComponents/WeatherData.tsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const WeatherDataComponent=({city}:any)=>{
+
+    const [temp,setTemp]=useState("")
+    const [feels_like,setFeels_like]=useState("")
+    const [humidity,setHumidity]=useState("")
+
+   async function readTemperature(){
+         let response=  await  axios.get(
+             "https://api.openweathermap.org/data/2.5/weather?q="+
+                city+"&APPID=84537705ffe70fde1b945b4254dff0c7")
+          let result=response.data.main
+          setTemp(Math.round(Number(result.temp)-273)+" Centigrade")
+          setHumidity(result.humidity)
+          setFeels_like(Math.round(Number(result.feels_like)-273)+" Centigrade")
+    }
+
+
+    useEffect(()=>{
+        readTemperature()
+    },[city])
 
     return(
         <div className="card" 
@@ -7,9 +29,9 @@ const WeatherDataComponent=({city}:any)=>{
         color:"#212F3C"}}>
             <h1 className="title">{city}</h1>
             <ul>
-                <li>Temperature - Would be filled</li>
-                <li>Feels Like - Would be Filled</li>
-                <li>Humidty - Would be filled</li>
+                <li>Temperature - {temp}</li>
+                <li>Feels Like -{feels_like}</li>
+                <li>Humidty - {humidity}</li>
             </ul>
         </div>
     )
